ebdf8fb15950031b09c9b6a65bdaf826c8dcc6a0
Karo <karo.game37@gmail.com>
2025-10-09 19:56:59 -0400

    chore: add cpp file for windows application

--- RenderingEngine/include/rendering/application/WindowsApplication.h
+++ RenderingEngine/include/rendering/application/WindowsApplication.h
@@ -15,37 +15,11 @@ public:
 	{
 	}
 
-	bool Init()
-	{
-		if (!RegisterWindowClass())
-			return false;
-
-		if (!CreateMainWindow())
-			return false;
-
-		ShowWindow(mainWindow, SW_SHOW);
-		UpdateWindow(mainWindow);
-
-		return true;
-	}
+	bool Init();
 
 	[[nodiscard]] HWND& GetMainWindow() { return mainWindow; }
 
-	static bool ProcessWindowMessages()
-	{
-		MSG msg;
-
-		while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
-		{
-			if (msg.message == WM_QUIT)
-				return false;
-
-			TranslateMessage(&msg);
-			DispatchMessageW(&msg);
-		}
-
-		return true;
-	}
+	[[nodiscard]] static bool ProcessWindowMessages();
 
 private:
 	HINSTANCE instance{};
@@ -54,81 +28,11 @@ private:
 	LPCWSTR className{};
 	LPCWSTR windowTitle{};
 
-	bool CreateMainWindow()
-	{
-		mainWindow = CreateWindowExW
-		(
-			0,
-			className,
-			windowTitle,
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, CW_USEDEFAULT,
-			CW_USEDEFAULT, CW_USEDEFAULT,
-			nullptr,
-			nullptr,
-			instance,
-			this
-		);
+	[[nodiscard]] bool CreateMainWindow();
+	[[nodiscard]] ATOM RegisterWindowClass() const;
 
-		return mainWindow != nullptr;
-	}
-
-	[[nodiscard]] ATOM RegisterWindowClass() const
-	{
-		WNDCLASSEXW wcex{};
-
-		wcex.cbSize = sizeof(wcex);
-		wcex.style = CS_HREDRAW | CS_VREDRAW;
-		wcex.lpfnWndProc = &WindowsApplication::HandleWindowsMessage;
-		wcex.hInstance = instance;
-		wcex.hIcon = LoadIconW(nullptr, IDI_APPLICATION);
-		wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
-		wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
-		wcex.lpszClassName = className;
-		wcex.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
-
-		return RegisterClassExW(&wcex);
-	}
-
-	[[nodiscard]] static LRESULT ProcessWindowMessage(const HWND hWnd, const UINT msg, const WPARAM wp, const LPARAM lp)
-	{
-		static constexpr LRESULT MESSAGE_HANDLED = 0;
-
-		switch (msg)
-		{
-		case WM_PAINT:
-			PAINTSTRUCT ps;
-
-			BeginPaint(hWnd, &ps);
-			EndPaint(hWnd, &ps);
-
-			break;
-		case WM_DESTROY:
-			PostQuitMessage(MESSAGE_HANDLED);
-			break;
-		default:
-			return DefWindowProcW(hWnd, msg, wp, lp);
-		}
-
-		return MESSAGE_HANDLED;
-	}
-
-	static LRESULT CALLBACK HandleWindowsMessage(const HWND hWnd, const UINT message, const WPARAM wParam,
-	                                              const LPARAM lParam)
-	{
-		if (message == WM_NCCREATE)
-		{
-			const auto cs = reinterpret_cast<CREATESTRUCTW*>(lParam);
-			SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(cs->lpCreateParams));
-
-			return DefWindowProcW(hWnd, message, wParam, lParam);
-		}
-
-		if (const auto self = reinterpret_cast<WindowsApplication*>(GetWindowLongPtrW(hWnd, GWLP_USERDATA)))
-			return ProcessWindowMessage(hWnd, message, wParam, lParam);
-
-		return DefWindowProcW(hWnd, message, wParam, lParam);
-	}
+	[[nodiscard]] static LRESULT ProcessWindowMessage(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+	[[nodiscard]] static LRESULT CALLBACK HandleWindowsMessage(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 };
 
 #endif
---
+++ RenderingEngine/src/rendering/application/WindowsApplication.cpp
@@ -0,0 +1,108 @@
+#include "pch.h"
+#include "rendering/application/WindowsApplication.h"
+
+bool WindowsApplication::Init()
+{
+	if (!RegisterWindowClass())
+		return false;
+
+	if (!CreateMainWindow())
+		return false;
+
+	ShowWindow(mainWindow, SW_SHOW);
+	UpdateWindow(mainWindow);
+
+	return true;
+}
+
+bool WindowsApplication::ProcessWindowMessages()
+{
+	MSG msg;
+
+	while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+	{
+		if (msg.message == WM_QUIT)
+			return false;
+
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+
+	return true;
+}
+
+bool WindowsApplication::CreateMainWindow()
+{
+	mainWindow = CreateWindowExW
+	(
+		0,
+		className,
+		windowTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		nullptr,
+		nullptr,
+		instance,
+		this
+	);
+
+	return mainWindow != nullptr;
+}
+
+ATOM WindowsApplication::RegisterWindowClass() const
+{
+	WNDCLASSEXW wcex{};
+
+	wcex.cbSize = sizeof(wcex);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = &WindowsApplication::HandleWindowsMessage;
+	wcex.hInstance = instance;
+	wcex.hIcon = LoadIconW(nullptr, IDI_APPLICATION);
+	wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+	wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+	wcex.lpszClassName = className;
+	wcex.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
+
+	return RegisterClassExW(&wcex);
+}
+
+LRESULT WindowsApplication::ProcessWindowMessage(const HWND hWnd, const UINT msg, const WPARAM wp, const LPARAM lp)
+{
+	static constexpr LRESULT MESSAGE_HANDLED = 0;
+
+	switch (msg)
+	{
+	case WM_PAINT:
+		PAINTSTRUCT ps;
+
+		BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(MESSAGE_HANDLED);
+		break;
+	default:
+		return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+
+	return MESSAGE_HANDLED;
+}
+
+LRESULT WindowsApplication::HandleWindowsMessage(const HWND hWnd, const UINT message, const WPARAM wParam,
+                                                 const LPARAM lParam)
+{
+	if (message == WM_NCCREATE)
+	{
+		const auto cs = reinterpret_cast<CREATESTRUCTW*>(lParam);
+		SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(cs->lpCreateParams));
+
+		return DefWindowProcW(hWnd, message, wParam, lParam);
+	}
+
+	if (const auto self = reinterpret_cast<WindowsApplication*>(GetWindowLongPtrW(hWnd, GWLP_USERDATA)))
+		return ProcessWindowMessage(hWnd, message, wParam, lParam);
+
+	return DefWindowProcW(hWnd, message, wParam, lParam);
+}
