1b75b09253e75bcd68b6f4e048d6b0ad85be7729
Karo <karo.game37@gmail.com>
2025-11-07 13:54:20 -0500

    feat: add texture sampling

--- RenderingEngine/src/rendering/texture/TextureLoader.cpp
+++ RenderingEngine/src/rendering/texture/TextureLoader.cpp
@@ -14,7 +14,7 @@ Texture TextureLoader::LoadTextureFromFile(const std::string& filePath, ID3D11De
 
 	return
 	{
-		.texture = CreateTextureView(device, texture, textureDesc),
+		.texture = CreateTextureView(device, texture),
 		.width = static_cast<UINT>(pictureData.width),
 		.height = static_cast<UINT>(pictureData.height)
 	};
@@ -29,7 +29,7 @@ ComPtr<ID3D11ShaderResourceView> TextureLoader::LoadTextureFromMemory(
 
 	stbi_image_free(pictureData.data);
 
-	return CreateTextureView(device, texture, textureDesc);
+	return CreateTextureView(device, texture);
 }
 
 TextureLoader::PictureData TextureLoader::LoadPictureFromFile(const std::string& filePath)
@@ -61,34 +61,37 @@ D3D11_TEXTURE2D_DESC TextureLoader::CreateTextureDesc(const PictureData& picture
 
 	desc.Width = pictureData.width;
 	desc.Height = pictureData.height;
-	desc.MipLevels = 1;
+	desc.MipLevels = 0;
 	desc.ArraySize = 1;
 	desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	desc.SampleDesc.Count = 1;
 	desc.Usage = D3D11_USAGE_DEFAULT;
-	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+	desc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 	desc.CPUAccessFlags = 0;
 
 	return desc;
 }
 
 ComPtr<ID3D11Texture2D> TextureLoader::CreateTexture(ID3D11Device* device, const PictureData& pictureData,
-                                                     const D3D11_TEXTURE2D_DESC& desc)
+	const D3D11_TEXTURE2D_DESC& desc)
 {
-	ComPtr<ID3D11Texture2D> texture{};
-	D3D11_SUBRESOURCE_DATA subResource;
-	subResource.pSysMem = pictureData.data;
-	subResource.SysMemPitch = desc.Width * 4;
-	subResource.SysMemSlicePitch = 0;
+	ComPtr<ID3D11Texture2D> texture;
 
-	DXEssayer(device->CreateTexture2D(&desc, &subResource, &texture), "Failed to create 2D texture");
+	DXEssayer(device->CreateTexture2D(&desc, nullptr, &texture), "Failed to create 2D texture");
+
+	ID3D11DeviceContext* context = nullptr;
+	device->GetImmediateContext(&context);
+	context->UpdateSubresource(texture, 0, nullptr, pictureData.data, pictureData.width * 4, 0);
+
+	ComPtr<ID3D11ShaderResourceView> srv = CreateTextureView(device, texture);
+	context->GenerateMips(srv);
 
 	return texture;
 }
 
 ComPtr<ID3D11ShaderResourceView> TextureLoader::CreateTextureView(ID3D11Device* device,
-                                                                  const ComPtr<ID3D11Texture2D>& texture,
-                                                                  const D3D11_TEXTURE2D_DESC& desc)
+                                                                  const ComPtr<ID3D11Texture2D>& texture)
 {
 	ComPtr<ID3D11ShaderResourceView> textureView{};
 	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
@@ -96,10 +99,13 @@ ComPtr<ID3D11ShaderResourceView> TextureLoader::CreateTextureView(ID3D11Device* 
 
 	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MipLevels = desc.MipLevels;
+	srvDesc.Texture2D.MipLevels = -1;
 	srvDesc.Texture2D.MostDetailedMip = 0;
 
-	DXEssayer(device->CreateShaderResourceView(texture, &srvDesc, &textureView), "Failed to create texture view");
+	DXEssayer(
+		device->CreateShaderResourceView(texture, &srvDesc, &textureView), 
+		"Failed to create texture view"
+	);
 
 	return textureView;
 }
