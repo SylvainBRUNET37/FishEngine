35eaa1456446fc9bc3054f0da0b76e5ddbd22318
Karo <karo.game37@gmail.com>
2025-11-19 18:16:10 -0500

    feat: add red color on the vignette effect

--- Need4Fish/shaders/PostProcessPS.hlsl
+++ Need4Fish/shaders/PostProcessPS.hlsl
@@ -19,21 +19,26 @@ SamplerState samp : register(s0);
 // Vignette effect: https://www.youtube.com/watch?v=GiQ5OvDN8dE
 static const float radius = 0.8; // distance from the center where the effect begins
 static const float softness = 0.5; // transition softness between the zone with and without the effect
-static const float strength = 0.5; // intensity of the color (it's like an alpha channel)
+static const float strength = 0.3; // intensity of the color (it's like an alpha channel)
+static const float3 color = float3(1.0, 0.0, 0.0);
 
 float4 PostProcessPS(VSOutput input) : SV_TARGET
 {
     float2 position = input.uv * 2.0 - 1.0;
     float distance = length(position);
 
-    // The vignette effect
+    // The vignette mask
     float vignetteMask = smoothstep(radius, radius + softness, distance);
 
     // Retrieve the color of the pixel of the scene
-    float4 vignetteColor = sceneTexture.Sample(samp, input.uv);
+    float4 pixelColor = sceneTexture.Sample(samp, input.uv);
 
-    // Apply vignette
-    vignetteColor.rgb *= lerp(1.0, 1.0 - strength, vignetteMask);
+    // Apply the dark factor of the vignette
+    float darkFactor = lerp(1.0, 1.0 - strength, vignetteMask);
 
-    return vignetteColor;
+	// Tint the vignette with a color
+    float3 tintedEdges = lerp(pixelColor.rgb, color, vignetteMask * strength);
+    pixelColor.rgb = tintedEdges * darkFactor;
+
+    return pixelColor;
 }
