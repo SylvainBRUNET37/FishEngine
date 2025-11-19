81bb8dbb7d92be6a7005a29ed64257e65ff60e97
Nell15 <nelltruong67@gmail.com>
2025-11-18 20:00:54 -0500

    refactor (GameEngine): moved the eating logic to its own file

--- Need4Fish/src/GameEngine.cpp
+++ Need4Fish/src/GameEngine.cpp
@@ -9,10 +9,7 @@
 #include "Locator.h"
 #include "rendering/application/WindowsApplication.h"
 #include "rendering/texture/TextureLoader.h"
-#include <Jolt/Physics/Collision/Shape/ScaledShape.h>
-
-#include <iostream>
-#include <fstream>
+#include "gameplay/mechanics/Eating.h"
 
 using namespace DirectX;
 
@@ -93,115 +90,13 @@ void GameEngine::HandleGameState()
 }
 
 void GameEngine::HandleCollions() {
-	auto isBodyEatable = [&](JPH::BodyID bodyId) -> std::optional<std::tuple<const Entity, Eatable&, RigidBody&>> {
-		auto eatables = entityManager.View<Eatable, RigidBody>();
-		auto it = std::find_if(eatables.begin(), eatables.end(),
-			[&](auto&& tuple)
-			{
-				auto& [entity, eatable, rigidBody] = tuple;
-				return rigidBody.body->GetID() == bodyId;
-			});
-		if (it != eatables.end())
-		{
-			return std::tuple<Entity, Eatable&, RigidBody&>(*it);
-		}
-		return std::nullopt;
-	};
-
-	auto isEntityAPlayer = [&](Entity SearchedEntity) -> bool {
-		auto watchables = entityManager.View<Controllable>();
-		auto it = std::find_if(watchables.begin(), watchables.end(),
-			[&](auto&& tuple)
-			{
-				auto& [entity, _] = tuple;
-				return entity.index == SearchedEntity.index;
-			});
-		return it != watchables.end();
-		};
-
+	
 	while (!GameState::detectedCollisions.empty())
 	{
 		auto& [bodyId1, bodyId2] = GameState::detectedCollisions.front();
 		GameState::detectedCollisions.pop();
-
-		// Check if the both objects is eatable
-		auto firstObject = isBodyEatable(bodyId1);
-		auto secondObject = isBodyEatable(bodyId2);
-		if (firstObject.has_value() && secondObject.has_value())
-		{
-			auto& [firstEntity, firstEatable, firstBody] = firstObject.value();
-			auto& [secondEntity, secondEatable, secondBody] = secondObject.value();
-
-			// Kill things if necessary
-			if (firstEatable.CanBeEatenBy(secondEatable)) {
-				if (isEntityAPlayer(firstEntity))
-				{
-					//InitGame();
-					GameState::currentState = GameState::DIED;
-				}
-				else
-				{
-					if (firstEatable.isApex) GameState::currentState = GameState::WON;
-					entityManager.Kill(firstEntity);
-					secondEatable.mass += firstEatable.mass;
-
-					// Shape scaling test
-					auto currentShape = secondBody.body->GetShape();
-					auto& bodyInterface = JoltSystem::GetPhysicSystem().GetBodyInterface();
-					JPH::Vec3 scale(2.0f, 2.0f, 2.0f);   // test scale // TODO changer ça + fonction
-					if (currentShape->GetSubType() == JPH::EShapeSubType::Box)
-					{
-						const JPH::BoxShape* box = static_cast<const JPH::BoxShape*>(currentShape);
-
-						JPH::Vec3 newHalfExtents = box->GetHalfExtent() * scale;
-
-						JPH::RefConst<JPH::Shape> newShape = new JPH::BoxShape(newHalfExtents);
-
-						bodyInterface.SetShape(bodyId2, newShape, true, JPH::EActivation::Activate);
-					}
-					// Scale mesh
-					auto& trans = entityManager.Get<Transform>(secondEntity);
-					trans.scale.x *= 2;
-					trans.scale.y *= 2;
-					trans.scale.z *= 2;
-				}
-			}
-			else if (secondEatable.CanBeEatenBy(firstEatable)) {
-				if (isEntityAPlayer(secondEntity))
-				{
-					//InitGame();
-					GameState::currentState = GameState::DIED;
-				}
-				else
-				{
-					if (secondEatable.isApex) GameState::currentState = GameState::WON;
-					entityManager.Kill(secondEntity);
-					firstEatable.mass += secondEatable.mass;
-
-					// Shape scaling test
-					auto currentShape = firstBody.body->GetShape();
-					auto& bodyInterface = JoltSystem::GetPhysicSystem().GetBodyInterface();
-					JPH::Vec3 scale(2.0f, 2.0f, 2.0f);   // test scale // TODO: changer ça + fonction
-					if (currentShape->GetSubType() == JPH::EShapeSubType::Box)
-					{
-						const JPH::BoxShape* box = static_cast<const JPH::BoxShape*>(currentShape);
-
-						JPH::Vec3 newHalfExtents = box->GetHalfExtent() * scale;
-
-						JPH::RefConst<JPH::Shape> newShape = new JPH::BoxShape(newHalfExtents);
-
-						bodyInterface.SetShape(bodyId1, newShape, true, JPH::EActivation::Activate);
-					}
-					// Scale mesh
-					auto& mesh = entityManager.Get<Mesh>(firstEntity);
-					auto& trans = entityManager.Get<Transform>(firstEntity);
-					trans.scale.x *= 2;
-					trans.scale.y *= 2;
-					trans.scale.z *= 2;
-				}
-			}
-			if (GameState::currentState != GameState::PLAYING) ChangeGameStatus();
-		}
+		Eating::Eat(entityManager, bodyId1, bodyId2);
+		if (GameState::currentState != GameState::PLAYING) ChangeGameStatus();
 	}
 }
 
