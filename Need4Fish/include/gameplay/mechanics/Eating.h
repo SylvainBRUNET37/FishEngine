5228b566bd9667e03922c943e4843c1c2a656740
Nell15 <nelltruong67@gmail.com>
2025-11-19 00:03:39 -0500

    feat: mosasaurus gradually grows

--- Need4Fish/include/gameplay/mechanics/Eating.h
+++ Need4Fish/include/gameplay/mechanics/Eating.h
@@ -4,4 +4,6 @@
 namespace Eating
 {
 	void Eat(EntityManager& entityManager, JPH::BodyID bodyId1, JPH::BodyID bodyId2);
+
+	void UpdatePlayerScale(EntityManager& entityManager);
 }
--- RenderingEngine/include/rendering/core/Transform.h
+++ RenderingEngine/include/rendering/core/Transform.h
@@ -9,6 +9,7 @@ struct Transform
     DirectX::XMFLOAT3 position{ 0, 0, 0 };
     DirectX::XMFLOAT4 rotation{ 0, 0, 0, 1 };
     DirectX::XMFLOAT3 scale{ 1, 1, 1 };
+    DirectX::XMFLOAT3 deltaScale{ 0, 0, 0 };
 };
 
 #endif
