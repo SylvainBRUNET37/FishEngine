838726238eed17e510d9ee56ef0d3647d8638e4b
Nell15 <nelltruong67@gmail.com>
2025-12-07 13:18:04 -0500

    chore(ai): bigger fishes do not panic

--- Need4Fish/include/components/Components.h
+++ Need4Fish/include/components/Components.h
@@ -37,7 +37,7 @@ struct AIController
 {
 	float maxSpeed = 50.0f; // TODO: tmp
 	float acceleration = 10.0f;
-	float safeDistance = 10.0f;
+	float safeDistance = 500.0f;
 	float updateDelay = 1.0f;
 	float remainingDelay = 0.0f;
 };
